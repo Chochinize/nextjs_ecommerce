2432070c227ba333b730e676647d2fcb154b9c38
chochiniz <chochinize@gmail.com>
2022-01-02 04:19:32 +0100

    toast   color

--- components/Toast.js
+++ components/Toast.js
@@ -1,7 +1,7 @@
 const Toast = ({msg, handleShow, bgColor}) => {
   return(
       <div className={`toast show position-fixed text-light ${bgColor}`}
-      style={{ top: '5px', right: '5px', zIndex: 9, minWidth: '280px', height:'300px' }} >
+      style={{ top: '5px', right: '5px', zIndex: 9, minWidth: '280px', height:'100px' }} >
 
           <div className={`toast-header ${bgColor} text-light`}>
               <strong className="mr-auto text-light">{msg.title}</strong>
