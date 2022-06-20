18f96ea4fa665c3639f2c56ff36e4fb5f43a54c6
Martin Aleksiev <chochinize@gmail.com>
2022-06-20 17:02:15 +0200

    secrets

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
   reactStrictMode: true,
-  env:{
-       "BASE_URL":'https://nextjs-ecommerce-six-sigma.vercel.app/',
-       "MONGODB_URL": 'mongodb+srv://Martin:Martin1234@cluster0.nordd.mongodb.net/Connections?retryWrites=true&w=majority',
-       "ACCESS_TOKEN_SECRET":"martin11279984157400015",
-       "REFRESH_TOKEN_SECRET":"martin123secret31726265rere948235005",
-       "PAYPAL_CLIENT_ID":"AWzldE_G57W17bhSFTU4gxZRfRs5hyR60BlDHB6bekpz3IxoqnV6ca3E6kbtoabYym2cuk1puFBD7K19",
-       "CLOUD_UPDATE_PRESET":"nextjs_store",
-       "CLOUD_NAME":"dydzpejva",
-       "CLOUD_API":"https://api.cloudinary.com/v1_1/dydzpejva/image/upload"
-  }
+  env:{}
 }
+// https://nextjs-ecommerce-six-sigma.vercel.app/
